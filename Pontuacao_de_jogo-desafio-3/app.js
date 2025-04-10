68fa7cb4fa3342b54c30d079b47cb5dfbda4d7ae
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-10 20:26:32 -0300

    Sistema de contabilização de Pontos
    Finalizado o exercício de contabilizar pontos

---
+++ Pontuacao_de_jogo-desafio-3/app.js
@@ -0,0 +1,9 @@
+alert(`Olá! Seja bem vindo ao Sistema de contabilização de pontos!`)
+
+let pontuação = prompt(`Qual foi a sua pontuação no jogo?`);
+if (pontuação >= 100){
+    alert (`Parabens Pela pontuação, foi aprovado.`);
+    console.log(`Sua pontuação foi o suficiente!`);
+}else (
+    alert(`Sua pontuação não foi o suficiente para conseguir!`)
+)
