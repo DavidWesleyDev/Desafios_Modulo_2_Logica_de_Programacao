e0a0e94226018c62c543f2493e7c8e77bc9303bd
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-10 19:54:39 -0300

    Dia da Semana
    Finalizando exercícios de fixação

---
+++ dias_da_semana-desafio-1/app.js
@@ -0,0 +1,9 @@
+let diaDaSemana = prompt('qual o dia da semana?');
+
+if(diaDaSemana == `Sábado `){
+    alert(`Bom fim de semana`);
+}else if(diaDaSemana == 'Domingo'){
+    alert (`Bom fim de semana!`);
+}else {
+    alert(`Boa semana! `)
+};
